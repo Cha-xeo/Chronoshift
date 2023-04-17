57de29c1d945bae64a4fd5d075f5805d0723871d
Cha-xeo <75042650+Cha-xeo@users.noreply.github.com>
2023-04-18 04:52:00 +0900

    multi

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,66 +6,21 @@ using UnityEngine.Experimental.VFX;
 using UnityEngine.VFX;
 using UnityEngine.Tilemaps;
 using Unity.VisualScripting;
+using Unity.Netcode;
 
-public class PlayerController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class PlayerController : NetworkBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-    
-    bool clicked = false;
-    [SerializeField] VisualEffect _vfx;
-    [SerializeField] InputHandler _input;
-    [SerializeField] Tilemap _tilemap;
-    [SerializeField] Grid _grid;
-    public int ViewRayon;
-    Color _color;
+    //[SerializeField] VisualEffect _vfx;
     public bool cl = false;
-
-    private void Start()
-    {
-        _color = new Color(1,1,1,1);
-    }
-
-    private readonly Vector3Int[] neighbourPositions =
-    {
-        Vector3Int.up,
-        Vector3Int.right,
-        Vector3Int.down,
-        Vector3Int.left,
-        Vector3Int.up + Vector3Int.right,
-        Vector3Int.up + Vector3Int.left,
-        Vector3Int.down + Vector3Int.right,
-        Vector3Int.down + Vector3Int.left
-    };
 
     private void Update()
     {
-        transform.Translate(_input.InputVector * 2 * Time.deltaTime);
-        _vfx.SetVector3("Sphere pos", transform.position);
+        if (!IsOwner) return;
+        transform.Translate(InputManager.GetInstance().GetMoveDirection() * 2 * Time.deltaTime);
+        //_vfx.SetVector3("Sphere pos", transform.position);
         //_tilemap.SetColor(_tilemap.WorldToCell(transform.position), cl ? Color.black : Color.white);
     }
 
-    private void FixedUpdate()
-    {
-        //UpdateFog();
-    }
-
-    private void UpdateFog()
-    {
-        Vector3 posBuf = transform.position;
-        /*for (int i = 0; i < ViewRayon; i++)
-        {
-            _tilemap.SetColor(_tilemap.WorldToCell(transform.position), Color.white);
-        }*/
-        foreach (var neighbourPosition in neighbourPositions)
-        {
-            Vector3Int position = _tilemap.WorldToCell(posBuf + neighbourPosition);
-
-            //_tilemap.cel
-            if (_tilemap.HasTile(position))
-            {
-                _tilemap.SetColor((position), cl ? Color.black : Color.white);
-            }
-        }
-    }
     public virtual void OnPointerEnter(PointerEventData eventData)
     {
         Debug.Log("Enter");
